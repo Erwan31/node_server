1773d9e6f73867da74b0e34f4add1c80de11ba6a
Erwan31 <spilmonterwan@gmail.com>
2020-08-19 18:59:20 +0100

    Manipulation of middleware

--- serverNode.js
+++ serverNode.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware
+app.use('/css', express.static(__dirname + "public/css"));
+app.use('/', function( req, res, next){
+    console.log(`Someone made a request: ${req.url}`);
+    res.cookie('cookinename', '45454');
+    next();
+})
+
 
 app.get('/', function( req, res){
     res.send(`
     <html>
+        <head>
+         <link type="text/css" rel="stylesheet" href="/css/style.css">
+        </head>
         <body>
-            <h1 style="background:red;">HEllo there!!</h1>
+            <h1>HEllo there!!</h1>
         </body>
     </html>
     `);
@@ -23,12 +36,15 @@ app.get('/api/user', function(req, res){
 app.get('/api/user/:id', function( req, res){
     res.send(`
     <html>
+        <head>
+            <link type="text/css" rel="stylesheet" href="/css/style.css" />
+        </head>
         <body>
-            <h1 style="background:red;">${req.params.id}</h1>
+            <h1>${req.params.id}</h1>
         </body>
     </html>
     `);
-});
+}); 
 
 
 // Query
