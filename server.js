41415d6611501149ef89bd73d36de21f753e13ab
Erwan31 <spilmonterwan@gmail.com>
2020-08-19 17:20:19 +0100

    Hand routing

--- server.js
+++ server.js
@@ -11,13 +11,24 @@ const cars = {
 }
 
 const server = http.createServer( function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/json'});
-
-    const json = JSON.stringify({
-        names,
-        cars
-    })
-    res.end(json);
+    
+    if(req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(HTML);
+    }
+    else if( req.url === "/api/user"){
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        const json = JSON.stringify({
+                names,
+                cars
+            });
+            res.end(json);
+    }
+    else{
+        res.writeHead(404);
+        res.end();
+    }
+    
 })
 
 server.listen(8181, '127.0.0.1');
